64a72acce0463c57752b7b0420062b28341f3cf9
ShirajShrestha <shresthashiraj333@gmail.com>
2023-10-12 19:22:38 +0545

    Added Music Player

---
+++ Mini Project/MusicPlayer/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+    {
+      name: "After Like",
+      artist: "IVE",
+      img: "After Like",
+      src: "After Like"
+    },
+    {
+      name: "DUMB DUMB",
+      artist: "Somi",
+      img: "DUMB DUMB",
+      src: "DUMB DUMB"
+    },
+    {
+      name: "Hype Boy",
+      artist: "NewJeans",
+      img: "Hype Boy",
+      src: "Hype Boy"
+    },
+    {
+      name: "I AM",
+      artist: "IVE",
+      img: "I AM",
+      src: "I AM"
+    },
+    {
+      name: "LOVE DIVE",
+      artist: "IVE",
+      img: "LOVE DIVE",
+      src: "LOVE DIVE"
+    },
+    {
+      name: "OMG",
+      artist: "NewJeans",
+      img: "OMG",
+      src: "OMG"
+    },
+    
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
